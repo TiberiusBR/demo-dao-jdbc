6549a85db88ab42b45b057475deac826ebdc6bd3
TiberiusBR <felipecs_98@hotmail.com>
2021-03-04 10:17:46 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,11 +31,18 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		/*
 		System.out.println("\nTEST 4 - seller Insert");
 		Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(), 4000.0, dep);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
-
+		*/
+		
+		System.out.println("\nTEST 5 - seller update");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed.");
 	}
 
 }
